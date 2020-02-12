92f7334d17dee41248d859fcf25492c72f963dac
asynts <asynts@gmail.com>
2020-02-12 17:44:05 +0100

    Remove foreign key from ApplicationUser.
    https://discordapp.com/channels/634104110131445811/635510829499940912/677193121217642496

--- src/Infrastructure/Identity/ApplicationUser.cs
+++ src/Infrastructure/Identity/ApplicationUser.cs
@@ -4,9 +4,6 @@ namespace Codidact.Authentication.Infrastructure.Identity
 {
     public class ApplicationUser : IdentityUser<long>
     {
-        /// <summary>
-        /// Foreign key for the 'Member' table in the 'core' application.
-        /// </summary>
-        public int MemberId { get; set; }
+
     }
 }
