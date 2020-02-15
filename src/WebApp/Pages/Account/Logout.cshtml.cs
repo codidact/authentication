d2f80ef2df722a5d995fb8379f8bed508783e1e8
Paul Scharnofske <asynts@gmail.com>
2020-02-15 17:48:00 +0100

    Add logout functionality.

---
+++ src/WebApp/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,41 @@
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Identity;
+
+using IdentityServer4.Services;
+using IdentityServer4.Events;
+using IdentityServer4.Extensions;
+
+using Codidact.Authentication.Domain.Entities;
+
+namespace Codidact.Authentication.WebApp.Pages.Account
+{
+    public class LogoutModel : PageModel
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IEventService _events;
+
+        public LogoutModel(
+            SignInManager<ApplicationUser> signInManager,
+            IEventService events)
+        {
+            _signInManager = signInManager;
+            _events = events;
+        }
+
+        public string ReturnUrl { get; set; } = "/index";
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (User?.Identity.IsAuthenticated ?? false)
+            {
+                await _signInManager.SignOutAsync();
+                await _events.RaiseAsync(new UserLogoutSuccessEvent(User.GetSubjectId(), User.GetDisplayName()));
+            }
+
+            return Redirect(ReturnUrl);
+        }
+    }
+}
