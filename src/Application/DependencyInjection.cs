fb47bd828c895e6a0c6099a3b6adbca8dece7dd5
Paul Scharnofske <asynts@gmail.com>
2020-02-22 11:35:06 +0100

    Remove first-use page and do not create an administrator from code.
    It makes no sense to create an administrator like this. For the MVP we can
    simply execute a query manually to add an administrator, post MVP we can create
    a fancy first-use page where this can be done with a user interface.

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -1,16 +1,12 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 
-using Codidact.Authentication.Application.Services;
-
 namespace Codidact.Authentication.Application
 {
     public static class DependencyInjection
     {
         public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddScoped<InitializationService>();
-
             return services;
         }
     }
