0bca02c78e26518be931c71030eceb56a8072492
Paul Scharnofske <asynts@gmail.com>
2020-02-23 12:32:17 +0100

    Rename the identity tables to match our naming convention.

--- src/Infrastructure/Persistance/ApplicationDbContext.cs
+++ src/Infrastructure/Persistance/ApplicationDbContext.cs
@@ -13,5 +13,18 @@ namespace Codidact.Authentication.Infrastructure.Persistance
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>().ToTable("identity_user");
+            builder.Entity<IdentityRole<long>>().ToTable("identity_role");
+            builder.Entity<IdentityRoleClaim<long>>().ToTable("identity_role_claim");
+            builder.Entity<IdentityUserLogin<long>>().ToTable("identity_user_login");
+            builder.Entity<IdentityUserClaim<long>>().ToTable("identity_user_claim");
+            builder.Entity<IdentityUserRole<long>>().ToTable("identity_user_role");
+            builder.Entity<IdentityUserToken<long>>().ToTable("identity_user_token");
+        }
     }
 }
