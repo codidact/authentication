9be18400546caf110a9871b6c3c914640420f6d5
Paul Scharnofske <asynts@gmail.com>
2020-02-26 15:26:49 +0100

    Remove redundant build instructions, link to core instead.
    The CSRF stuff is now tracked in #5.

--- src/Client/Pages/Logout.cshtml.cs
+++ src/Client/Pages/Logout.cshtml.cs
@@ -7,8 +7,6 @@ namespace Codidact.Authentication.Client.Pages
     {
         public IActionResult OnGet()
         {
-            // Todo. Is this a CSRF vulnerbility? The 'oidc' schema should be fine,
-            // but the cookies are removed without a POST request, or not?
             return SignOut("cookie", "oidc");
         }
     }
