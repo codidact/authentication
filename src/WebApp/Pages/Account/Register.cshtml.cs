c26cc49abc33ab17d4a98b98ff020c1c2408e94b
Misha Tarnortusky <michael@sensory.co.il>
2020-03-18 15:15:39 -0400

    Add Register Page & User Creation

---
+++ src/WebApp/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Codidact.Authentication.Domain.Entities;
+using IdentityServer4.Events;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Codidact.Authentication.WebApp.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IEventService _events;
+
+        public RegisterModel(
+                          UserManager<ApplicationUser> userManager,
+                          IEventService events)
+        {
+            _userManager = userManager;
+            _events = events;
+        }
+
+        [Required, DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required, DataType(DataType.Text)]
+        public string DisplayName { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = "Confirm password doesn't match")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string ReturnUrl { get; set; } = "/index";
+        public void OnGet([FromQuery] string returnUrl)
+        {
+            if (returnUrl != null)
+            {
+                ReturnUrl = returnUrl;
+            }
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _userManager.CreateAsync(new ApplicationUser
+                {
+                    Email = Email,
+                    UserName = DisplayName,
+                }, Password);
+                if (result.Succeeded)
+                {
+                    return LocalRedirect(ReturnUrl);
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(error.Code, error.Description);
+                    }
+                }
+            }
+
+            return Page();
+        }
+    }
+}
