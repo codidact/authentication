a0abfe32a45fc2b05def5d1a184bd276a06eb444
weegee <somilsharma627@gmail.com>
2020-04-02 23:42:00 +0530

    Remove unused property

--- src/Domain/Entities/EmailSettings.cs
+++ src/Domain/Entities/EmailSettings.cs
@@ -6,7 +6,6 @@ namespace Codidact.Authentication.Domain.Entities
         public int Port { get; set; }
         public string SenderName { get; set; }
         public string Sender { get; set; }
-        public string Password { get; set; }
         public bool EnableSsl { get; set; }
     }
 }
