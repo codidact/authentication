30eecc663dd768490dbed47f5bba6f3f69a5ecb9
Michael Tarnorutsky <michael@sensory.co.il>
2020-04-09 06:02:27 -0400

    Add CoreApi Implementation (#23)

---
+++ tests/Infrastructure.UnitTests/Services/CoreApiTests/CoreApiServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+using Codidact.Authentication.Infrastructure.Services.CoreApi;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace Infrastructure.UnitTests.Services.CoreApiTests
+{
+    public class CoreApiServiceTest
+    {
+        [Theory]
+        [InlineData("http://localhost:8000")]
+        public async Task CreateMemberApiReturnsResult(string url)
+        {
+            var coreApiService = new CoreApiService(NullLogger<CoreApiService>.Instance);
+
+            var result = await coreApiService.CreateMemberAsync(url, "John", 1);
+
+            Assert.True(result.Success);
+        }
+    }
+}
